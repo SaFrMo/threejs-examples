1ab711420c04f2b488c04655efc07c6229f5c170
Sander <sander@funkhaus.us>
2018-12-19 16:21:30 -0500

    Starting examples

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,60 @@
 <template>
-    <main class="class"></main>
+    <main class="three-examples">
+        <h2>Examples</h2>
+
+        <ul class="grid">
+            <li>
+                <!-- <span class="title">Breathing Tower</span> -->
+                <div class="wrap"><purple-tower /></div>
+            </li>
+        </ul>
+    </main>
 </template>
 
 <script>
-export default {}
+import PurpleTower from './examples/PurpleTower'
+
+export default {
+    components: {
+        'purple-tower': PurpleTower
+    }
+}
 </script>
 
 <style lang="scss">
-.class {
+@import 'src/styles/vars';
+
+.three-examples {
+    font-family: $font-family;
+
+    .grid {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+
+        li {
+            width: 200px;
+            height: 200px;
+            position: relative;
+            border: 1px solid $black;
+            border-radius: 10px;
+            // background: rgba($color4, 0.1);
+            display: flex;
+            flex-direction: column;
+            overflow: hidden;
+
+            .title {
+                border-bottom: 1px solid $black;
+                padding: 8px 5px 10px;
+                font-family: $header-font;
+                text-transform: lowercase;
+                font-weight: 700;
+            }
+            .wrap {
+                position: relative;
+                flex: 1;
+            }
+        }
+    }
 }
 </style>
---
+++ src/examples/PurpleTower.vue
@@ -0,0 +1,52 @@
+<template>
+    <vue-three-wrap :start="start" :update="update" />
+</template>
+
+<script>
+import VueThreeWrap from 'vue-three-wrap'
+import * as THREE from 'three'
+import { transform } from 'popmotion'
+
+const ref = {}
+
+export default {
+    components: { 'vue-three-wrap': VueThreeWrap },
+    methods: {
+        start({ scene, camera }) {
+            // add light
+            ref.dirLight = new THREE.DirectionalLight(0xffffff, 1)
+            ref.dirLight.position.set(1, 0, 1)
+            scene.add(ref.dirLight)
+
+            // example adding a cube to the scene
+            ref.cubes = []
+            const cubeCount = 20
+            const rainbow = transform.interpolate(
+                [0, cubeCount - 1],
+                ['hsl(200, 100%, 50%)', 'hsl(340, 100%, 50%)']
+            )
+            for (let i = 0; i < cubeCount; i++) {
+                const geometry = new THREE.BoxGeometry(0.5, 1, 0.5)
+                const material = new THREE.MeshLambertMaterial({
+                    color: rainbow(i)
+                })
+                const cube = new THREE.Mesh(geometry, material)
+                // cube.position.x = -8
+                cube.position.y = i - 10
+                ref.cubes.push(cube)
+                scene.add(cube)
+            }
+
+            // move camera
+            camera.position.z = 10
+        },
+        update() {
+            ref.cubes.forEach((cube, i) => {
+                cube.rotation.y -= 0.0002
+                cube.scale.x = cube.scale.z =
+                    Math.abs(Math.sin(Date.now() * 0.001 + i / 10) * 5) + 5
+            })
+        }
+    }
+}
+</script>
