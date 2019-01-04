44a077e2da712e48bd1d51151c9130f1e31359be
Sander <sander@funkhaus.us>
2019-01-04 11:44:58 -0500

    Added cartoon pipe effect

--- src/examples/CartoonPipe.vue
+++ src/examples/CartoonPipe.vue
@@ -12,13 +12,28 @@
             void main() {
                 vUv = uvScale * uv;
 
-                // build extruded position
-                vec3 extrude = normal * sin(time * 0.001) ;
-                vec3 pos = position + extrude;
+                // get extruded position
+                vec3 extrude = normal * 5.;
+
+                // current time in seconds (ie, 1.234)
+                float timeInSeconds = time / 1000.;
+                // speed of bulge movement
+                float speed = 0.5;
+                // number of divisions of this tube
+                // (higher = more bulges in cartoon pipe)
+                float divisions = 2.;
+                // current coordinate, from 0 to 1
+                float coord = fract((uv.x - timeInSeconds * speed) * divisions);
+                // the higher the number, the stronger the bulge effect
+                float bulgeExaggeration = 10.;
+                // get intensity of current position
+                float intensity = pow(smoothstep(0., 1., 1. - distance(coord, 0.5)), bulgeExaggeration);
+
+                // build final position by mixing default position and extruded position
+                vec3 pos = mix(position, position + extrude, intensity);
 
                 vec4 mvPosition = modelViewMatrix * vec4( pos, 1.0 );
                 vNormal = normal;
-
 
                 gl_Position = projectionMatrix * mvPosition;
             }
@@ -47,6 +62,24 @@ import * as THREE from 'three'
 const ref = {}
 let mountTime = 0
 
+// copied from https://threejs.org/docs/index.html#api/en/geometries/TubeGeometry
+function CustomSinCurve(scale) {
+    THREE.Curve.call(this)
+
+    this.scale = scale === undefined ? 1 : scale
+}
+
+CustomSinCurve.prototype = Object.create(THREE.Curve.prototype)
+CustomSinCurve.prototype.constructor = CustomSinCurve
+
+CustomSinCurve.prototype.getPoint = function(t) {
+    var tx = t * 10 - 5
+    var ty = Math.sin(2 * Math.PI * t) * 0.5
+    var tz = 0
+
+    return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale)
+}
+
 export default {
     components: {
         'vue-three-wrap': VueThreeWrap
@@ -54,9 +87,8 @@ export default {
     methods: {
         async start({ scene, camera }) {
             mountTime = Date.now()
-
-            camera.position.z = 10
-            camera.position.y = 0.75
+            camera.position.y = 5
+            camera.position.z = 50
 
             // uniforms
             const uniforms = {
@@ -74,10 +106,11 @@ export default {
                     .textContent
             })
 
-            // sphere
-            const geo = new THREE.CylinderGeometry(2, 2, 15, 32)
+            // pipe
+            const path = new CustomSinCurve(10)
+            const geo = new THREE.TubeGeometry(path, 64, 4, 8, false)
             ref.pipe = new THREE.Mesh(geo, shaderMat)
-            ref.pipe.rotateZ(Math.PI / 2)
+            // ref.pipe.rotateZ(Math.PI)
             scene.add(ref.pipe)
         },
         update() {
