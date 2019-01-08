4589967db350b0e933d2ea1a88f8c372a0a48a42
Sander <sander@funkhaus.us>
2019-01-08 14:50:11 -0500

    Arrows working

--- src/examples/PointTest2.vue
+++ src/examples/PointTest2.vue
@@ -7,10 +7,9 @@ import VueThreeWrap from 'vue-three-wrap'
 import * as THREE from 'three'
 import { PerlinNoise } from '../utils/shared'
 import { wrap } from '@popmotion/popcorn'
+import Simplex from 'perlin-simplex'
 
-// Copied from https://codepen.io/EastingAndNorthing/pen/QpYWQq?editors=0010
-
-const ref = { particles: [] }
+const ref = {}
 
 export default {
     components: {
@@ -18,14 +17,57 @@ export default {
     },
     data() {
         return {
-            particleSpeed: 0.01,
+            // particles
+            particleSpeed: 0.1,
             size: 5,
-            numParticles: 2000
+            numParticles: 2000,
+
+            // noise
+            noiseScale: 10,
+            noiseOffset: 0,
+            noiseSpeed: 0.01,
+            simplex: new Simplex(),
+
+            // wind
+            windFieldWidth: 5,
+            windFieldHeight: 5
         }
     },
     methods: {
         start({ camera, scene }) {
+            // move camera back
             camera.position.z = 3
+
+            // "wind" will be a 1d array of Vector2s, from top left to bottom right
+            ref.wind = []
+            for (let y = 0; y < this.windFieldWidth; y++) {
+                for (let x = 0; x < this.windFieldHeight; x++) {
+                    const noise =
+                        (this.simplex.noise(
+                            x * this.noiseScale,
+                            y * this.noiseScale
+                        ) +
+                            1) /
+                        2
+                    const vector = new THREE.Vector3(noise, 0, 0)
+                    ref.wind.push(vector)
+                    const arrow = new THREE.ArrowHelper(
+                        vector.clone().normalize(),
+                        new THREE.Vector3(
+                            this.size * (x / this.windFieldWidth) -
+                                this.size / 2,
+                            this.size * (y / this.windFieldHeight) -
+                                this.size / 2,
+                            0.1
+                        ),
+                        vector.x,
+                        0x0000ff,
+                        0.1,
+                        0.1
+                    )
+                    scene.add(arrow)
+                }
+            }
 
             // geometry (this will just be an array of Vector3s for points)
             const geo = new THREE.Geometry()
@@ -60,17 +102,24 @@ export default {
         },
         update({ scene }) {
             // update each vertex
-            ref.points.geometry.vertices.forEach(v => {
-                // build new position
-                v.add(new THREE.Vector3(this.particleSpeed, 0, 0))
-                // build position wrapper
-                const w = wrap(-this.size / 2, this.size / 2)
-                // wrap to ensure we stay in bounds
-                v.set(w(v.x), w(v.y), w(v.z))
-            })
-
-            // apply updates
-            ref.points.geometry.verticesNeedUpdate = true
+            // ref.points.geometry.vertices.forEach(v => {
+            //     const delta =
+            //         this.simplex.noise(v.x, v.y) *
+            //         this.noiseScale *
+            //         this.particleSpeed
+            //
+            //     this.noiseOffset += this.noiseSpeed
+            //
+            //     // build new position
+            //     v.add(new THREE.Vector3(Math.abs(delta), 0, 0))
+            //     // build position wrapper
+            //     const w = wrap(-this.size / 2, this.size / 2)
+            //     // wrap to ensure we stay in bounds
+            //     v.set(w(v.x), w(v.y), w(v.z))
+            // })
+            //
+            // // apply updates
+            // ref.points.geometry.verticesNeedUpdate = true
         }
     }
 }
