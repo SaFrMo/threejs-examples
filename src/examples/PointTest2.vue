ccd02a7ddde214c3efc1926da70b2f499f85c4e7
Sander <sander@funkhaus.us>
2019-01-08 13:57:42 -0500

    Cleaner set of points

--- src/examples/PointTest2.vue
+++ src/examples/PointTest2.vue
@@ -5,60 +5,12 @@
 <script>
 import VueThreeWrap from 'vue-three-wrap'
 import * as THREE from 'three'
+import { PerlinNoise } from '../utils/shared'
+import { wrap } from '@popmotion/popcorn'
 
 // Copied from https://codepen.io/EastingAndNorthing/pen/QpYWQq?editors=0010
 
 const ref = { particles: [] }
-let geo = null
-let mat = null
-
-class Particle {
-    constructor(x, y, z, vm, scene) {
-        this.pos = new THREE.Vector3(x, y, z)
-        this.vel = new THREE.Vector3(0, 0, 0)
-        this.acc = new THREE.Vector3(0, 0, 0)
-        this.vel = new THREE.Euler(0, 0, 0)
-        this.mesh = new THREE.Points(geo, mat)
-        this.vm = vm
-
-        // TODO: is this necessary?
-        this.mesh.geometry.dynamic = true
-        this.mesh.geometry.verticesNeedUpdate = true
-        this.acc.multiplyScalar()
-
-        scene.add(this.mesh)
-    }
-
-    // update
-    update() {
-        // set acceleration to 1,1,1
-        this.acc.set(1, 1, 1)
-        // TODO: what does applyEuler actually change?
-        this.acc.applyEuler(this.angle)
-        // TODO: what happens when these change?
-        this.acc.multiplyScalar(this.vm.noiseStrength)
-
-        // clamp min and max values
-        this.acc.clampLength(0, this.vm.particleSpeed)
-        this.vel.clampLength(0, this.vm.particleSpeed)
-
-        // apply acceleration
-        this.vel.add(this.acc)
-        // apply transformation
-        this.pos.add(this.vel)
-
-        // keep particles in bounds
-        if (this.pos.x > this.vm.size) this.pos.x = 0 + Math.random()
-        if (this.pos.y > this.vm.size) this.pos.y = 0 + Math.random()
-        if (this.pos.z > this.vm.size) this.pos.z = 0 + Math.random()
-        if (this.pos.x < 0) this.pos.x = this.vm.size - Math.random()
-        if (this.pos.y < 0) this.pos.y = this.vm.size - Math.random()
-        if (this.pos.z < 0) this.pos.z = this.vm.size - Math.random()
-
-        // move mesh!
-        this.mesh.position.set(this.pos.x, this.pos.y, this.pos.z)
-    }
-}
 
 export default {
     components: {
@@ -66,50 +18,59 @@ export default {
     },
     data() {
         return {
-            noiseStrength: 0.8,
-            particleSpeed: 0.1,
-            size: 50,
-
-            numParticles: 200
+            particleSpeed: 0.01,
+            size: 5,
+            numParticles: 2000
         }
     },
     methods: {
-        start({ scene, camera }) {
-            camera.position.y = 0.75
-            camera.position.z = 10
+        start({ camera, scene }) {
+            camera.position.z = 3
 
-            // add light
-            // scene.add(new THREE.HemisphereLight(0xff0033, 0x9900ff, 1))
-
-            // geometry
-            geo = new THREE.Geometry()
+            // geometry (this will just be an array of Vector3s for points)
+            const geo = new THREE.Geometry()
             geo.vertices.push(new THREE.Vector3(0, 0, 0))
 
             // material
-            mat = new THREE.PointsMaterial({
-                color: '#ff0000',
+            const mat = new THREE.PointsMaterial({
+                color: '#cc0000',
                 size: 0.2,
                 transparent: true,
-                opacity: 0.4,
+                opacity: 0.3,
                 blending: THREE.AdditiveBlending
             })
-        },
-        update({ scene }) {
-            if (mat == null) {
-                return
+
+            // add initial points
+            for (let i = 0; i < this.numParticles; i++) {
+                geo.vertices.push(
+                    new THREE.Vector3(
+                        THREE.Math.randFloatSpread(this.size),
+                        THREE.Math.randFloatSpread(this.size),
+                        0
+                        // THREE.Math.randFloat(0, this.size / 2)
+                    )
+                )
             }
 
-            // make sure we have enough particles
-            while (ref.particles.length < this.numParticles) {
-                const p = new Particle(
-                    THREE.Math.randFloatSpread(this.size),
-                    THREE.Math.randFloatSpread(this.size),
-                    THREE.Math.randFloatSpread(this.size),
-                    this,
-                    scene
-                )
-                ref.particles.push(p)
-            }
+            // save points
+            ref.points = new THREE.Points(geo, mat)
+
+            // add points to scene
+            scene.add(ref.points)
+        },
+        update({ scene }) {
+            // update each vertex
+            ref.points.geometry.vertices.forEach(v => {
+                // build new position
+                v.add(new THREE.Vector3(this.particleSpeed, 0, 0))
+                // build position wrapper
+                const w = wrap(-this.size / 2, this.size / 2)
+                // wrap to ensure we stay in bounds
+                v.set(w(v.x), w(v.y), w(v.z))
+            })
+
+            // apply updates
+            ref.points.geometry.verticesNeedUpdate = true
         }
     }
 }
